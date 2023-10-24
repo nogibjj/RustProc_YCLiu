0727acc556f66471e85e64426df9b61b41f317bb
Yun-Chung LIU <46064664+halfmoonliu@users.noreply.github.com>
2023-10-24 21:38:10 +0000

    fix code length

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def testCalSpeed():
     for num in test_l:
         Amount += num
     time_end = time.perf_counter()
-    
-    print(f"Added 100,000,000 numbers within {time_end - time_start:0.4f} seconds")
-    print("CPU Usage: {:.2}%".format(psutil.cpu_percent(interval=time_end - time_start)))
+    time_dif = time_end - time_start
+    print(f"Added 100,000,000 numbers within {time_dif} s")
+    print("CPU Usage: {:.2}%".format(psutil.cpu_percent(interval=time_dif)))
     return Amount
 
 if __name__ == "__main__":
